75a97e9373ba2969a2ba0ca8e20ca41ac6052ae9
hguesne <hguesne@student.42lehavre.fr>
2026-09-15 18:03:13 +0200

    add stay connected

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,19 @@
 // Link = créer un lien cliquable sans recharger la page
 // src/pages/Home.tsx
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export const Home = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    
+    // Si un token est présent, on redirige automatiquement vers le profil ou le jeu
+    if (token) {
+      navigate("/game");
+    }
+  }, [navigate]);
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center gap-4">
       <h1 className="text-4xl font-bold">Bienvenue sur Transcendance</h1>
