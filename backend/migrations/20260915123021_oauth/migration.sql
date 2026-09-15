2fc46ae010b92e046a5de64d24dfc4fc70a1348f
Benjamin <beaurainbenjamin1@gmail.com>
2026-09-15 15:52:10 +0200

    resolve conflits

--- backend/migrations/20260915123021_oauth/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[oauthProvider,oauthId]` on the table `User` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "User_oauthProvider_oauthId_key" ON "User"("oauthProvider", "oauthId");
