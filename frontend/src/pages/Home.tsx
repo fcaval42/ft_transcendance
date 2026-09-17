b56e5b4722f8f47147b8fa949224064f2540263f
hguesne <hguesne@student.42lehavre.fr>
2026-09-17 16:31:05 +0200

    header responsive

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -14,7 +14,7 @@ export const Home = () => {
   }
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center gap-4">
-      <h1 className="text-4xl font-bold">Bienvenue sur Transcendance</h1>
+      <h1 className="text-4xl font-bold items-center justify-center">Bienvenue sur Transcendance</h1>
       <Link
         to="/login"
         className="bg-indigo-400 text-white px-6 py-3 rounded-lg hover:bg-indigo-500 transition-colors"
