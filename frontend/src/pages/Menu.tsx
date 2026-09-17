fe0323d1191c650e9c8df59efb11c3c4dc06b8f0
Fleur Caval <fcaval@c1r1p4.42lehavre.fr>
2026-09-17 11:57:48 +0200

    ajout page menu+bouton déconnection

--- frontend/src/pages/Menu.tsx
+++ frontend/src/pages/Menu.tsx
@@ -2,26 +2,33 @@ import { Link } from "react-router-dom";
 
 export const Menu = () => {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center gap-6">
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center gap-6 relative">
+      
+      {/* Bouton déconnexion en haut à droite */}
+      <button className="absolute top-4 right-4 bg-yellow-400 text-white px-4 py-2 rounded-lg
+      hover:bg-yellow-500 transition-colors">
+        Se déconnecter
+      </button>
+
       <h1 className="text-4xl font-bold mb-8">Menu Principal</h1>
 
       <Link
-        to="/pvp-bot"
-        className="bg-peru-500 text-white px-8 py-4 rounded-lg hover:bg-peru-600 transition-colors text-xl w-64 text-center flex items-center justify-center gap-2"
+        to="/game"
+        className="bg-red-300 text-white px-8 py-4 rounded-lg hover:bg-red-400 transition-colors text-xl w-64 text-center flex items-center justify-center gap-2"
       >
         <span>🤖</span> Player vs Bot
       </Link>
 
       <Link
         to="/pvp"
-        className="bg-peachpuff-500 text-white px-8 py-4 rounded-lg hover:bg-peachpuff-600 transition-colors text-xl w-64 text-center flex items-center justify-center gap-2"
+        className="bg-sky-300 text-white px-8 py-4 rounded-lg hover:bg-sky-400 transition-colors text-xl w-64 text-center flex items-center justify-center gap-2"
       >
         <span>👥</span> Player vs Player
       </Link>
 
       <Link
         to="/tournament"
-        className="bg-purple-500 text-white px-8 py-4 rounded-lg hover:bg-purple-600 transition-colors text-xl w-64 text-center flex items-center justify-center gap-2"
+        className="bg-violet-300 text-white px-8 py-4 rounded-lg hover:bg-violet-400 transition-colors text-xl w-64 text-center flex items-center justify-center gap-2"
       >
         <span>🏆</span> Tournoi
       </Link>
