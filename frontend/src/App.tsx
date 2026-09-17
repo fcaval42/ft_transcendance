f76fc30a2a2258b4fae91cc31451880740416f58
hguesne <hguesne@student.42lehavre.fr>
2026-09-17 10:48:02 +0200

    route protege

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ProtectedRoute, UnlogRoute } from "./utils/checkRoute";
 import { Login } from "./pages/Login";
 import { Game } from "./pages/Game";
 import { Home } from "./pages/Home";
@@ -8,13 +9,19 @@ function App() {
   return (
     <Router>
       <Routes>
+        { /* Route par défaut (page d'accueil) */}
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/game" element={<Game />} />
+        { /* Route pour la page de connexion */}
+        <Route element={<UnlogRoute />}>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+        { /* Route disponibles si connecté */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/game" element={<Game />} />
+        </Route>
       </Routes>
     </Router>
   );
 }
-
 export default App;
