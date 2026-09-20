ce51e7b34d8658a1315de5889a9b449e64c0d8f3
Slashal <hguesne@student.42lehavre.fr>
2026-09-20 22:37:03 +0200

    trad menu ok

--- frontend/src/pages/Menu.tsx
+++ frontend/src/pages/Menu.tsx
@@ -1,25 +1,27 @@
 import { Link } from "react-router-dom";
 import { Header } from "../components/Header";
+import { useTranslation } from "react-i18next";
 
 export const Menu = () => {
+  const { t } = useTranslation();
   return (
       <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center gap-6 relative">
         <Header />
 
-      <h1 className="text-4xl font-bold mb-8">Menu Principal</h1>
+      <h1 className="text-4xl font-bold mb-8">{t("menu.menu")}</h1>
 
       <Link
         to="/game"
         className="bg-red-300 text-white px-8 py-4 rounded-lg hover:bg-red-400 transition-colors text-xl w-64 text-center flex items-center justify-center gap-2"
       >
-        <span>🤖</span> Player vs Bot
+        <span>🤖</span> {t("menu.vsBot")}
       </Link>
 
       <Link
         to="/pvp"
         className="bg-sky-300 text-white px-8 py-4 rounded-lg hover:bg-sky-400 transition-colors text-xl w-64 text-center flex items-center justify-center gap-2"
       >
-        <span>👥</span> Player vs Player
+        <span>👥</span> {t("menu.vsPlayer")}
       </Link>
     </div>
   );
